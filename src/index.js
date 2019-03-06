43863d531d577e3147324585b992b223654f559e
Айнур-ПК\Айнур <felix-xilef@mail.ru>
2019-03-06 12:39:35 +0300

    add check is data string

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
+  if (typeof str !== 'string') {
+    alert('Данные не ввиде строки!');
+  }
+
   counter = 0;  //баланс правильной последовательности
   for (var i = 0; i < str.length; i++) {
     if (str[i] == '(') {
