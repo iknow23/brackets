f74d8b01408c10dc31585dcea3039e150416ec9d
Айнур-ПК\Айнур <felix-xilef@mail.ru>
2019-03-06 12:12:54 +0300

    Add valid sequence check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  counter = 0;  //баланс правильной последовательности
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] == '(') {
+      counter++;
+    } else {
+      counter--;
+    }
+  }
+  
+  if (counter < 0) {
+    return false;
+  }
+  return counter == 0;
 }
